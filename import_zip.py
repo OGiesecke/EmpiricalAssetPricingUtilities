888e36caf95d8458e882ef24d5496b5534532410
OGiesecke <o.giesecke@gmail.com>
2020-12-22 21:53:16 -0500

    update

---
+++ import_zip.py
@@ -0,0 +1,6 @@
+def import_zip(url,skiprows_v=0):
+    r = requests.get(url)
+    zf = zipfile.ZipFile(BytesIO(r.content))
+    print("Imported " + zipfile.ZipFile.namelist(zf)[0])
+    df = pd.read_csv(zf.open(zipfile.ZipFile.namelist(zf)[0]), skiprows=skiprows_v)
+    return df
