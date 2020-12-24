6ac8fcbee6ed9b1e5b5908717e233375d2323240
OGiesecke <o.giesecke@gmail.com>
2020-12-23 21:26:48 -0500

    update

--- import_df.py
+++ import_df.py
@@ -1,5 +1,18 @@
-import pandas as pd
 import numpy as np
+import zipfile
+import io
+from io import BytesIO
+import pandas as pd
+import requests
+
+def import_zip(url,skiprows_v=0):
+    r = requests.get(url)
+    zf = zipfile.ZipFile(BytesIO(r.content))
+    print("Imported " + zipfile.ZipFile.namelist(zf)[0])
+    df = pd.read_csv(zf.open(zipfile.ZipFile.namelist(zf)[0]), skiprows=skiprows_v)
+    return df
+
+
 
 def import_df(url,name):
     data = import_zip(url,skiprows_v=3).rename(columns={"Unnamed: 0":"date"})
