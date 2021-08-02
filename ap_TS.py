b8405d294f19d5ea499c04e32101a43a8208a4f7
OGiesecke <o.giesecke@gmail.com>
2021-08-03 01:27:12 +0200

    add one factor possibility

--- ap_TS.py
+++ ap_TS.py
@@ -9,6 +9,7 @@ def ap_TS(mr,mf):
     dT, dN = mr.shape
     dT, dK = mf.shape
 
+    vbeta = np.empty((dN,dK))
     valpha = np.empty((dN,1))
     valpha_t = np.empty((dN,1))
     mresid = np.empty((dT,dN))
@@ -19,17 +20,23 @@ def ap_TS(mr,mf):
     for i in range(0,dN):
         vres = newey(mr[:,i],np.hstack((vones, mf)).reshape(dT,dK+1),0)
         valpha[i] = vres.beta[0]
+        vbeta[i,:] = vres.beta[1:]
         valpha_t[i] = vres.tstat[0]
         mresid[:,i] = vres.resid
 
     ## Properties of risk premia
 
     vlambda = np.mean(mf,0).transpose();
-    vlambda_t = vlambda / np.sqrt(np.diag(np.cov(mf,rowvar=0))/dT)
+    if dK == 1:
+        vlambda_t = vlambda / np.sqrt(np.var(mf)/dT)
+    else:
+        vlambda_t = vlambda / np.sqrt(np.diag(np.cov(mf,rowvar=0))/dT)
 
     ## GRS test
-
-    dGRS = ((dT-dN-dK)/dN)*1/(1+np.mean(mf,0)@np.linalg.inv(np.cov(mf,rowvar=0,bias=True))@np.mean(mf,0).transpose()) * valpha.transpose()@np.linalg.inv(np.cov(mresid,rowvar=0,bias=True))@valpha
+    if dK == 1:
+        dGRS = ((dT-dN-dK)/dN)*1/(1+np.mean(mf,0) / np.var(mf) * np.mean(mf,0)) * valpha.transpose()@np.linalg.inv(np.cov(mresid,rowvar=0,bias=True))@valpha
+    else:
+        dGRS = ((dT-dN-dK)/dN)*1/(1+np.mean(mf,0)@np.linalg.inv(np.cov(mf,rowvar=0,bias=True))@np.mean(mf,0).transpose()) * valpha.transpose()@np.linalg.inv(np.cov(mresid,rowvar=0,bias=True))@valpha
     dGRS_p = 1-f.cdf(dGRS,dN,dT-dN-dK);
 
-    return valpha, valpha_t, vlambda, vlambda_t, dGRS, dGRS_p
+    return valpha, valpha_t, vlambda, vlambda_t, dGRS, dGRS_p, vbeta
